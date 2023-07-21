ffa3dfd3d9bdd12f8170d7e05c451758ea99c297
Cosmas Kiplagat <kigen@techforensics.com>
2023-07-21 15:08:14 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+
+	/**
+	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
+	int main(void)
+	{
+	    times_table();
+	    return (0);
+	}
+
+
+
+=======================================
+
+
+10-add.c CODE
+
+
+#include "main.h"
+
+
+	/**
+	 * add - adds two integers and returns the result
+	 * @a: int to be added to b
+	 * @b: int to be added to a
+	 * Return: sum of a and b
+	 */
+	int add(int a, int b)
+	{
+		int c;
+
+
+		c = a + b;
+
+
+		return (c);
+	}
+
