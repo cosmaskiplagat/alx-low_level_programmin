f1fc5c6eb72f8a8d1e7987250c1e061e0e59ce22
Cosmas Kiplagat <kigen@techforensics.com>
2023-07-25 10:52:55 -0700

    redone putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,22 @@
 #include "main.h"
-
 /**
  * main - entry point
- *
  * Description:
  * This function is the entry point of the program. It prints the characters
  * '_putchar' to the standard output (stdout) using the _putchar function.
- *
  * Return:
  * Always 0.
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
+	_putchar('_');
+    	_putchar('p');
+       	_putchar('u');
+	_putchar('t');
+       	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+        _putchar('r');
+        _putchar('\n');
+	return (0);
 }
