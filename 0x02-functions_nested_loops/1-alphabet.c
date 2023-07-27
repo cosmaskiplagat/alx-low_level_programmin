ad218823a4fc4459ab3de706dc761089938734b2
Cosmas Kiplagat <kigen@techforensics.com>
2023-07-26 23:27:53 -0700

    _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,21 @@
 #include "main.h"
+/**
+ * main - entry point
+ * Description:
+ * This function is the entry point of the program. It prints the characters
+ * '_putchar' to the standard output (stdout) using the _putchar function.
+ * Return:
+ * Always 0.
+ */
+void print_alphabet(void)
+{
+    char letter;
 
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        _putchar(letter);
+    }
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
-	}
+    _putchar('\n');
+}
 
