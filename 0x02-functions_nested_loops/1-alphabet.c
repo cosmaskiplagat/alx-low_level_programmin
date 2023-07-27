848953a7c12cb9ffd8f9e78b74eb09687e1c00ef
Cosmas Kiplagat <kigen@techforensics.com>
2023-07-27 00:01:07 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,12 @@
 #include "main.h"
 /**
- * main - entry point
- * Description:
- * This function is the entry point of the program. It prints the characters
- * '_putchar' to the standard output (stdout) using the _putchar function.
- * Return:
- * Always 0.
+ * print_alphabet - make  alphabet
+ * Return:void
  */
 void print_alphabet(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
-
-    _putchar('\n');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
-
